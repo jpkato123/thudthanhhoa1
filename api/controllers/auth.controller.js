aedb11d4411cfa497517604fbc4c09bcb1d0cd13
jpkato <quanghieu.forever@gmail.com>
2024-05-02 16:12:32 +0900

    add middleware function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 import User from "../models/user.model.js"
 import bcryptjs from 'bcryptjs'
-export const signup = async(req, res) => {
+import { errorHandler } from "../utils/error.js"
+export const signup = async(req, res,next) => {
     const {username, email,password} = req.body
 
     if(!username || !email || !password || username === '' || email === '' || password ===''){
-        return res.status(400).json({message: 'All fields are required'})
+        // return res.status(400).json({message: 'All fields are required'})
+        return next(errorHandler(400, "All fields are required"));
     }
     const hashedPassword = bcryptjs.hashSync(password,10)
     const newUser = new User({
@@ -17,6 +19,7 @@ export const signup = async(req, res) => {
         res.status(200).json('signup successfully')
         
     } catch (error) {
-        res.status(500).json({message:error.message})
+        // res.status(500).json({message:error.message})
+        next(error)
     }
 }
--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import dotenv from "dotenv";
+import { error } from 'console';
 
 
 dotenv.config();
@@ -22,3 +23,12 @@ app.listen(3000,()=>{
 
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'internal server error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
