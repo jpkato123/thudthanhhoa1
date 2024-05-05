f7182e1093f98075c23fa126d80100e5f142a016
jpkato <quanghieu.forever@gmail.com>
2024-05-05 09:23:29 +0900

    user delete api completed

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,3 +53,14 @@ export const updateUser = async (req, res, next) => {
     next(error);
   }
 };
+export const deleteUser = async (req,res,next)=> {
+  if(req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'you are can not delete this user'))
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId)
+    res.status(200).json({message: 'User has been deleted'})
+  } catch (error) {
+   next(error) 
+  }
+}
