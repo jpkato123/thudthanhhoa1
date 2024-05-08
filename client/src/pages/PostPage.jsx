0fc8ccd38580a9582b05f791b3302de6a61e2a5b
jpkato <quanghieu.forever@gmail.com>
2024-05-08 22:20:07 +0900

    add comment section to post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,13 +1,15 @@
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Button } from "flowbite-react";
+import CallToAction from "../components/CallToAction";
+import CommentSection from "../components/CommentSection";
 
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [post, setPost] = useState(null);
-  //   console.log(post);
+    // console.log(post);
   useEffect(() => {
     const fetchPost = async () => {
       try {
@@ -55,6 +57,10 @@ export default function PostPage() {
       <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}>
         
       </div>
+      <div className="max-w-4xl mx-auto w-full">
+        <CallToAction/>
+      </div>
+      <CommentSection postId={post && post._id} />
     </main>
   );
 }
