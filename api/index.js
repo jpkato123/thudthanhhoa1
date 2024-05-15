0e5f318205ed64b57f1677deda778b60ba18983e
jpkato <quanghieu.forever@gmail.com>
2024-05-15 21:30:34 +0900

    add pre installation to deloy

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 dotenv.config();
 mongoose
@@ -16,6 +17,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+const __dirname = path.resolve();
 const app = express();
 app.use(express.json()); // cho phep hien thi json tren backend terminal
 app.use(cookieParser());
@@ -28,6 +30,12 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+app.get('*',(req,res)=>{
+  res.sendFile(path.join(__dirname,'client','dist','index.html'));
+});
+
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "internal server error";
