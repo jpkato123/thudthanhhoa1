56912064735aa9c86224f48841bb5f9a2db4ccab
jpkato <quanghieu.forever@gmail.com>
2024-05-15 22:43:10 +0900

    change facebook appid

--- client/src/components/FacebookMessger.jsx
+++ client/src/components/FacebookMessger.jsx
@@ -2,7 +2,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 
 export default function FacebookMessger() {
   return (
-    <FacebookProvider appId="127793563602289" chatSupport>
+    <FacebookProvider appId="460919496493783" chatSupport>
       <CustomChat pageId="120138041072452" minimized={true} />
     </FacebookProvider>
   );
